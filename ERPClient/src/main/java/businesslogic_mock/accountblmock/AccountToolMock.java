eb50b279e2b29284eb5517a73c49318abc8e8cd6
161250014 <161250014@mail.smal.nju.edu.cn>
2017-11-07 20:52:35 +0800

    删除了几个多余的文件夹

--- ERPClient/src/main/java/businesslogic_mock/accountblmock/AccountToolMock.java
+++ ERPClient/src/main/java/businesslogic_mock/accountblmock/AccountToolMock.java
@@ -7,7 +7,7 @@ import vo.account.AccountVO;
 
 import java.util.ArrayList;
 
-public class AccoutToolMock implements AccountTool{
+public class AccountToolMock implements AccountTool{
     @Override
     public ArrayList<AccountVO> getAccountList(AccountQueryVO query) {
         ArrayList<AccountVO> accountVOArrayList = new ArrayList<AccountVO>();
--- ERPClient/src/test/java/businesslogic_mocktester/accountblmocktester/AccoutToolMockTester.java
+++ ERPClient/src/test/java/businesslogic_mocktester/accountblmocktester/AccoutToolMockTester.java
@@ -2,13 +2,13 @@ package businesslogic_mocktester.accountblmocktester;
 
 import businesslogic.accountbl.AccountTool;
 import businesslogic.blutility.ResultMessage;
-import businesslogic_mock.accountblmock.AccoutToolMock;
+import businesslogic_mock.accountblmock.AccountToolMock;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class AccoutToolMockTester {
-    AccountTool accountTool = new AccoutToolMock();
+    AccountTool accountTool = new AccountToolMock();
     @Test
     public void getAccountList() throws Exception {
         assertEquals(1,accountTool.getAccountList(null).size());
