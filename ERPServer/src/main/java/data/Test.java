f79da53542f2a35767689c0cadbacf73ad2add7c
161250013 <2542745324@qq.com>
2017-12-07 22:12:10 +0800

    Service：修改了CashBillPO

--- ERPServer/src/main/java/data/Test.java
+++ ERPServer/src/main/java/data/Test.java
@@ -1,16 +1,15 @@
 package main.java.data;
 
-import main.java.data.initialdata.InitialData;
-import main.java.dataservice.initialdataservice.InitialDataService;
-import main.java.po.initial.InitialPO;
+import main.java.data.financedata.CashBillData;
+import main.java.dataservice.financedataservice.CashBillDataService;
+import main.java.po.bill.financebill.CashBillPO;
 
 import java.rmi.RemoteException;
-import java.util.ArrayList;
 
 public class Test {
     public static void main(String[] args) throws RemoteException {
         DataHelper.init();
-        InitialDataService service = new InitialData();
-        service.insert(new InitialPO(2017, new ArrayList<>(), new ArrayList<>(), new ArrayList<>(), new ArrayList<>()));
+        CashBillDataService service = new CashBillData();
+        service.insert(new CashBillPO());
     }
 }
--- ERPServer/src/main/java/po/bill/financebill/CashBillPO.java
+++ ERPServer/src/main/java/po/bill/financebill/CashBillPO.java
@@ -18,14 +18,23 @@ public class CashBillPO extends FinanceBillPO {
 
     }
 
-    public CashBillPO(String state, Date time, String operatorID, String comment, double total, ArrayList<CashItemPO> itemList) {
+    public CashBillPO(String state, Date time, String operatorID, String comment, double total, String accountID, ArrayList<CashItemPO> itemList) {
         this.state = state;
         this.time = time;
         this.operatorID = operatorID;
         this.comment = comment;
         type = "现金费用单";
         this.total = total;
+        this.accountID = accountID;
         this.itemList = itemList;
+    }
+
+    public String getAccountID() {
+        return accountID;
+    }
+
+    public void setAccountID(String accountID) {
+        this.accountID = accountID;
     }
 
     public ArrayList<CashItemPO> getItemList() {
