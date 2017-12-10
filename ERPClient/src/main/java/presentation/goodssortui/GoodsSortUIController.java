5df2c5f48e923f8250cf1ff1e16312b82699081a
161250014 <161250014@mail.smal.nju.edu.cn>
2017-12-10 20:49:22 +0800

    陈骁：添加一些库存人员的界面，请王颀涵修改BlService名称

---
+++ ERPClient/src/main/java/presentation/goodssortui/GoodsSortUIController.java
@@ -0,0 +1,154 @@
+package main.java.presentation.goodssortui;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.*;
+import main.java.MainApp;
+import main.java.businesslogic.goodssortbl.GoodsSortBl;
+import main.java.businesslogicservice.clientblservice.ClientBlService;
+import main.java.businesslogicservice.goodssortblservice.GoodsSortBLService;
+import main.java.presentation.clientui.ClientInfoUIController;
+import main.java.presentation.mainui.RootUIController;
+import main.java.presentation.messageui.InventoryPanelUIController;
+import main.java.presentation.messageui.PurchaseSalePanelUIController;
+import main.java.presentation.uiutility.CenterUIController;
+import main.java.vo.client.ClientVO;
+import main.java.vo.goods.GoodsSortVO;
+
+import java.util.ArrayList;
+
+public class GoodsSortUIController extends CenterUIController {
+    private GoodsSortBLService goodsSortBlService;
+    @FXML
+    private TreeView goodsSortTreeView;
+
+    // 加载文件后调用的方法******************************************
+
+    /**
+     * 设置显示的客户信息以及显示方法
+     * */
+    public void initialize(){
+        /*
+        root = new TreeItem<String>("Root");
+        root.setExpanded(true);
+        goodsSortTreeView.setRoot(root);
+        */
+    }
+
+    // 设置controller数据的方法*****************************************
+
+    public void setGoodsSortBlService(GoodsSortBLService goodsSortBlService) {
+        this.goodsSortBlService=goodsSortBlService;
+        //ArrayList<GoodsSortVO> goodsSortList=goodsSortBlService.getGoodsSortList(null);
+    }
+
+    private void showSortList(){
+        GoodsSortVO c1=new GoodsSortVO();
+        c1.setID("Sort12345");
+        c1.setName("二极管1");
+
+        GoodsSortVO c2=new GoodsSortVO();
+        c2.setID("Sort98085");
+        c2.setName("二极管2");
+
+        ArrayList<GoodsSortVO> list=new ArrayList<>();
+        list.add(c1);
+        list.add(c2);
+
+        GoodsSortVO b1=new GoodsSortVO();
+        b1.setID("Sort12345");
+        b1.setName("二极管");
+        b1.setChildren(list);
+
+        GoodsSortVO b2=new GoodsSortVO();
+        b2.setID("Sort98085");
+        b2.setName("白炽灯");
+
+        ArrayList<GoodsSortVO> list2=new ArrayList<>();
+        list2.add(b1);
+        list2.add(b2);
+
+        GoodsSortVO a=new GoodsSortVO();
+        a.setID("Sort12345678");
+        a.setName("灯具");
+        a.setChildren(list2);
+
+        addGoodsSort(null,a);
+    }
+
+    /**
+     * 取得客户列表并修改ObservableList的信息
+     * */
+    private void addGoodsSort(TreeItem<String> fatherItem,GoodsSortVO newSort){
+        TreeItem<String> newItem= new TreeItem<String>(newSort.getID()+": "+newSort.getName());
+
+        if(fatherItem==null){
+            goodsSortTreeView.setRoot(newItem);
+        }
+        else{
+            fatherItem.getChildren().add(newItem);
+        }
+
+        if(newSort.getChildren()!=null && newSort.getChildren().size()>0){
+            for(GoodsSortVO child:newSort.getChildren()){
+                addGoodsSort(newItem,child);
+            }
+        }
+    }
+
+    // 界面之中会用到的方法******************************************
+
+    @FXML
+    private void handleAddSort(){
+    }
+
+    @FXML
+    private void handleDeleteSort(){
+    }
+
+    @FXML
+    private void handleEditSort(){
+    }
+
+    @FXML
+    private void handleAddGoods() {
+    }
+
+    /*
+    private boolean isClientSelected(){
+    }
+    */
+
+    // 加载文件和界面的方法******************************************
+
+    /**
+     * 初始化方法，调用init方法
+     * 之所以有这个方法是为了多态而提供的
+     * */
+    public void instanceInit(RootUIController root){
+        init(root);
+    }
+
+    /**
+     * 静态初始化方法，加载相应的FXML文件，并添加一些信息
+     * */
+    public static void init(RootUIController root){
+        try{
+            // 加载登陆界面
+            FXMLLoader loader=new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("/main/java/presentation/goodssortui/GoodsSortUI.fxml"));
+            root.setCenterPane(loader.load());
+
+            GoodsSortUIController controller=loader.getController();
+            controller.setRoot(root);
+            controller.setGoodsSortBlService(new GoodsSortBl());
+
+            root.setReturnPaneController(new InventoryPanelUIController());
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
---
+++ ERPClient/src/main/java/presentation/goodsui/GoodsUIController.java
@@ -0,0 +1,160 @@
+package main.java.presentation.goodsui;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import main.java.MainApp;
+import main.java.businesslogicservice.goodsblservice.GoodsBLService;
+import main.java.presentation.mainui.RootUIController;
+import main.java.presentation.messageui.PurchaseSalePanelUIController;
+import main.java.presentation.uiutility.CenterUIController;
+import main.java.vo.goods.GoodsVO;
+import main.java.vo.goods.GoodsVO;
+
+import java.util.ArrayList;
+
+public class GoodsUIController extends CenterUIController {
+    private GoodsBLService goodsBlService;
+
+    private ObservableList<GoodsVO> goodsObservableList= FXCollections.observableArrayList();
+    @FXML
+    private TableView<GoodsVO> goodsTableView;
+    @FXML
+    private TableColumn<GoodsVO,String> goodsIDColumn;
+    @FXML
+    private TableColumn<GoodsVO,String> goodsNameColumn;
+    @FXML
+    private TableColumn<GoodsVO,String> goodsCostColumn;
+    @FXML
+    private TableColumn<GoodsVO,String> goodsRetailColumn;
+    @FXML
+    private TableColumn<GoodsVO,String> goodsNumberColumn;
+    @FXML
+    private TableColumn<GoodsVO,String> goodsCommentColumn;
+
+    // 加载文件后调用的方法******************************************
+
+    /**
+     * 设置显示的客户信息以及显示方法
+     * */
+    public void initialize(){
+        goodsIDColumn.setCellValueFactory(cellData->new SimpleStringProperty(cellData.getValue().getID()));
+        goodsNameColumn.setCellValueFactory(cellData->new SimpleStringProperty(cellData.getValue().getName()));
+        goodsCostColumn.setCellValueFactory(cellData->new SimpleStringProperty(String.valueOf(cellData.getValue().getCost())));
+        goodsRetailColumn.setCellValueFactory(cellData->new SimpleStringProperty(String.valueOf(cellData.getValue().getRetail())));
+        goodsNumberColumn.setCellValueFactory(cellData->new SimpleStringProperty(String.valueOf(cellData.getValue().getNumber())));
+        goodsCommentColumn.setCellValueFactory(cellData->new SimpleStringProperty(cellData.getValue().getComment()));
+    }
+
+    // 设置controller数据的方法*****************************************
+
+    public void setGoodsBlService(GoodsBLService goodsBlService) {
+        this.goodsBlService = goodsBlService;
+        //ArrayList<GoodsVO> goodsList=goodsBlService.getGoodsList(null);
+        //showGoodsList(goodsList);
+    }
+
+    /**
+     * 取得客户列表并修改ObservableList的信息
+     * */
+    private void showGoodsList(ArrayList<GoodsVO> goodsList){
+        goodsObservableList.removeAll();
+
+        for(int i=0;i<goodsList.size();i++){
+            goodsObservableList.add(goodsList.get(i));
+        }
+        goodsTableView.setItems(goodsObservableList);
+    }
+
+    // 界面之中会用到的方法******************************************
+
+
+    @FXML
+    private void handleDeleteGoods(){
+        int selectedIndex=goodsTableView.getSelectionModel().getSelectedIndex();
+        if(isGoodsSelected()){
+            goodsTableView.getItems().remove(selectedIndex);
+        }
+    }
+
+    @FXML
+    private void handleEditGoods(){
+        if(isGoodsSelected()){
+            //GoodsInfoUIController.init(goodsBlService,goodsTableView.getSelectionModel().getSelectedItem(),2,root.getStage());
+        }
+    }
+
+    @FXML
+    private void handleCheckGoods() {
+        if(isGoodsSelected()){
+            //GoodsInfoUIController.init(goodsBlService,goodsTableView.getSelectionModel().getSelectedItem(),3,root.getStage());
+        }
+    }
+
+    private boolean isGoodsSelected(){
+        int selectedIndex=goodsTableView.getSelectionModel().getSelectedIndex();
+        if(selectedIndex>=0){
+            return true;
+        }else{
+            // Nothing selected
+            Alert alert=new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("No Selection");
+            alert.setHeaderText("未选中商品");
+            alert.setContentText("请在表中选择商品");
+            alert.showAndWait();
+            return false;
+        }
+    }
+
+    // 加载文件和界面的方法******************************************
+
+    /**
+     * 初始化方法，调用init方法
+     * 之所以有这个方法是为了多态而提供的
+     * */
+    public void instanceInit(RootUIController root){
+        init(root);
+    }
+
+    /**
+     * 静态初始化方法，加载相应的FXML文件，并添加一些信息
+     * */
+    public static void init(RootUIController root){
+        try{
+            // 加载登陆界面
+            FXMLLoader loader=new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("/main/java/presentation/goodsui/GoodsUI.fxml"));
+            root.setCenterPane(loader.load());
+
+            GoodsUIController controller=loader.getController();
+            controller.setRoot(root);
+            controller.setGoodsBlService(null);
+
+            /*
+            GoodsVO c1=new GoodsVO("类别：经销商", 3, "名字：陈骁",
+                    "电话：123", "地址：南京大学", "邮编123", "邮件：123",
+            0, 0, 20, null);
+            c1.setID("123");
+            GoodsVO c2=new GoodsVO("类别：经销商", 4, "名字：陈骁2",
+                    "电话：123", "地址：南京大学", "邮编123", "邮件：123",
+                    0, 0, 20, null);
+            c2.setID("123");
+
+            ArrayList<GoodsVO> list=new ArrayList<>();
+            list.add(c1);
+            list.add(c2);
+
+            controller.showGoodsList(list);
+            */
+
+            root.setReturnPaneController(new PurchaseSalePanelUIController());
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
