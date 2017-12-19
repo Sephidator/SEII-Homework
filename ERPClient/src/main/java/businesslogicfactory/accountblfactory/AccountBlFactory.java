0d6bcdc70163148dab25ef33821b9ffc2da73b31
161250014 <161250014@mail.smal.nju.edu.cn>
2017-12-19 23:09:52 +0800

    Factory修改实例（看AccountBlFactory和AccountDataFactory)

--- ERPClient/src/main/java/businesslogicfactory/accountblfactory/AccountBlFactory.java
+++ ERPClient/src/main/java/businesslogicfactory/accountblfactory/AccountBlFactory.java
@@ -4,8 +4,13 @@ import main.java.businesslogic.accountbl.AccountBl;
 import main.java.businesslogicservice.accountblservice.AccountBlService;
 
 public class AccountBlFactory {
-    public AccountBlService getService(){
+    static public AccountBlService getService(){
         AccountBlService accountBlService=new AccountBl();
         return accountBlService;
     }
+    /*
+    * 给方法加上static，我之前忘加了
+    * 调用的时候，写法是：
+    * AccountBlService service=AccountBlFactory.getService();
+    * */
 }
