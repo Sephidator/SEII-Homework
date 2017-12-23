3140d205dafb61233d33631977942c3be33f40d5
161250013 <2542745324@qq.com>
2017-12-23 14:24:50 +0800

    修改initialPO的筛选属性

--- ERPServer/src/main/java/po/initial/InitialQueryPO.java
+++ ERPServer/src/main/java/po/initial/InitialQueryPO.java
@@ -3,11 +3,9 @@ package main.java.po.initial;
 import java.io.Serializable;
 
 public class InitialQueryPO implements Serializable {
-    public int start;//时间起点
-    public int end;//时间终点
+    public int year;//时间起点
 
-    public InitialQueryPO(int start, int end) {
-        this.start = start;
-        this.end = end;
+    public InitialQueryPO(int year) {
+        this.year = year;
     }
 }
