57dc31b56793fffffc5fb084e0b3ff01e9f3180b
161250013 <2542745324@qq.com>
2018-01-06 00:52:53 +0800

    修改了用例文档

--- ERPClient/src/main/java/client_blservicestub/inventoryblservicestub/InventoryCheckBlServiceStub.java
+++ ERPClient/src/main/java/client_blservicestub/inventoryblservicestub/InventoryCheckBlServiceStub.java
@@ -6,7 +6,7 @@ import main.java.vo.goods.InventoryCheckItemVO;
 import java.util.ArrayList;
 import java.util.Date;
 
-public class InventoryCheckBlServiceStuub implements InventoryCheckBlService {
+public class InventoryCheckBlServiceStub implements InventoryCheckBlService {
 
     @Override
     public ArrayList<InventoryCheckItemVO> getInventoryCheck(Date start, Date end) throws Exception {
